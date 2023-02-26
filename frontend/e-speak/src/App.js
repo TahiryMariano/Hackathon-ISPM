849cc205d62b5df1f75985b6212c9b6aa3a14636
Rixide <tsimaholisonricardo@gmail.com>
2023-02-27 02:24:06 +0300

    navbar implementation

--- frontend/e-speak/src/App.js
+++ frontend/e-speak/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav_container">
+      <div className="logo_container">
+        <h4>e-speak</h4>
+      </div>
+      <div className="links_container">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Courses</li>
+        </ul>
+      </div>
     </div>
   );
 }
