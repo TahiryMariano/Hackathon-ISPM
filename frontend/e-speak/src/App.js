a9bc43cd2da9e57c67000589dfd205e1d40f10ba
Rixide <tsimaholisonricardo@gmail.com>
2023-02-27 03:08:39 +0300

    Home page finish 2.0

--- frontend/e-speak/src/App.js
+++ frontend/e-speak/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Nav from "./component/Nav";
+import HomeBody from "./component/HomeBody";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <div className="home_body">
-        <h1>Let's learn foreign language</h1>
-        <button>Get started</button>
-      </div>
+      <HomeBody />
     </div>
   );
 }
---
+++ frontend/e-speak/src/component/HomeBody.js
@@ -0,0 +1,10 @@
+function HomeBody() {
+  return (
+    <div className="home_body">
+      <h1>Let's learn foreign language</h1>
+      <button>Get started</button>
+    </div>
+  );
+}
+
+export default HomeBody;
