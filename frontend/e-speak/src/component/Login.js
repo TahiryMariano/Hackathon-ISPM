7c76294fda1eafacd6dbbc9bb093e8e8e0d873ca
Rixide <tsimaholisonricardo@gmail.com>
2023-02-27 04:04:23 +0300

    template login finish

--- frontend/e-speak/src/component/Login.js
+++ frontend/e-speak/src/component/Login.js
@@ -2,24 +2,32 @@ function Login() {
   return (
     <div className="login_formulaire">
       <form action="">
-        <table>
-          <tr>
-            <td>
-              <label htmlFor="email">Email:</label>
-            </td>
-            <td>
-              <input type="email" name="email" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="password">Password:</label>
-            </td>
-            <td>
-              <input type="password" name="password" />
-            </td>
-          </tr>
-        </table>
+        <fieldset>
+          <legend>Login</legend>
+          <table cellSpacing="20px">
+            <tr>
+              <td>
+                <label htmlFor="email">Email:</label>
+              </td>
+              <td>
+                <input type="email" name="email" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="password">Password:</label>
+              </td>
+              <td>
+                <input type="password" name="password" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button>Sign In</button>
+              </td>
+            </tr>
+          </table>
+        </fieldset>
       </form>
     </div>
   );
